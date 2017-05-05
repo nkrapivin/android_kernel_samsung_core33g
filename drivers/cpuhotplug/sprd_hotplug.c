4121d76148ddfcfd706663aa6f6caeae996f36b4
HA$HT@G <hasherror404@gmail.com>
2017-05-05 20:42:08 +0530

    cpufreq: governors: Move get_governor_parent_kobj() and get_cpu_idle_time() to cpufreq.c
    Signed-off-by: HA$HT@G <hasherror404@gmail.com>

--- drivers/cpuhotplug/sprd_hotplug.c
+++ drivers/cpuhotplug/sprd_hotplug.c
@@ -205,40 +205,6 @@ struct cpufreq_conf {
 
 extern struct cpufreq_conf *sprd_cpufreq_conf;
 
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
-
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-	busy_time = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-		*wall = cputime_to_usecs(cur_wall_time);
-
-	return cputime_to_usecs(idle_time);
-}
-
-static inline u64 get_cpu_idle_time_sprd(unsigned int cpu, u64 *wall, int io_busy)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
-
-	if (idle_time == -1ULL)
-		return get_cpu_idle_time_jiffy(cpu, wall);
-	else if (!io_busy)
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
-
 static void __cpuinit sprd_plugin_one_cpu_ss(struct work_struct *work)
 {
 	int cpuid;
@@ -738,7 +704,7 @@ void dbs_check_cpu_sprd()
 		 * the iowait time to the cpu idle time.
 		 */
 		io_busy = g_sd_tuners->io_is_busy;
-		cur_idle_time = get_cpu_idle_time_sprd(j, &cur_wall_time, io_busy);
+		cur_idle_time = get_cpu_idle_time(j, &cur_wall_time, io_busy);
 
 		wall_time = (unsigned int)
 			(cur_wall_time - prev_cpu_wall);
@@ -1043,7 +1009,7 @@ static ssize_t store_io_is_busy(struct device *dev, struct device_attribute *att
 
 	/* we need to re-evaluate prev_cpu_idle */
 	for_each_online_cpu(j) {
-		g_prev_cpu_idle[j] = get_cpu_idle_time_sprd(j,
+		g_prev_cpu_idle[j] = get_cpu_idle_time(j,
 				&g_prev_cpu_wall[j],should_io_be_busy());
 	}
 	return count;
